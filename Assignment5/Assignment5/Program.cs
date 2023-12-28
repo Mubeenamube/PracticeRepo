3ffe74259c43a67d9870a5000c64438139679b6f
mubeena <95677268+Mubeenamube@users.noreply.github.com>
2023-12-28 23:03:08 +0530

    Update Test_Program.cs

--- Assignment5/Assignment5/Program.cs
+++ Assignment5/Assignment5/Program.cs
@@ -1,23 +1,25 @@
 ﻿namespace Assignment5
 {
+   internal class Program
+{
     interface GovtRules
     {
-        public double EmployeePF(double basicSalary);
-        public string LeaveDetails();
-        public double gratuityAmount(float serviceCompleted, double basicSalary);
+        double EmployeePF(double basicSalary);
+        string LeaveDetails();
+        double gratuityAmount(float serviceCompleted, double basicSalary);
 
     }
-    class TCS : GovtRules
+    public class TCS : GovtRules
     {
-        int empid;
-        string name;
-        string dept;
-        string desg;
-        double bs;
+        public int empid;
+        public string name;
+        public string dept;
+        public string desg;
+        public double bs;
 
-        public TCS(int empid,string name,string dept,string desg,double bs)
+        public TCS(int empid, string name, string dept, string desg, double bs)
         {
-            this.empid = empid;            
+            this.empid = empid;
             this.name = name;
             this.dept = dept;
             this.desg = desg;
@@ -35,28 +37,35 @@
         public double Bs
         { get { return bs; } }
 
-        double GovtRules.EmployeePF(double basicSalary)
+        public double EmployeePF(double basicSalary)
         {
-            throw new NotImplementedException();
+            return 0.12 * bs;
         }
 
-        double GovtRules.gratuityAmount(float serviceCompleted, double basicSalary)
+        public double gratuityAmount(float serviceCompleted, double basicSalary)
         {
-            throw new NotImplementedException();
+            if (serviceCompleted > 20)
+                return 3 * basicSalary;
+            else if (serviceCompleted > 10)
+                return 2 * basicSalary;
+            else if (serviceCompleted > 5)
+                return basicSalary;
+            else
+                return 0;
         }
 
-        string GovtRules.LeaveDetails()
+        public string LeaveDetails()
         {
-            throw new NotImplementedException();
+            return "Casual Leave: 1 day/month\nSick Leave: 12 days/year\nPrivilege Leave: 10 days/year";
         }
     }
-    class Accenture : GovtRules
+    public class Accenture : GovtRules
     {
-        int empid;
-        string name;
-        string dept;
-        string desg;
-        double bs;
+        public int empid;
+        public string name;
+        public string dept;
+        public string desg;
+        public double bs;
 
         public Accenture(int empid, string name, string dept, string desg, double bs)
         {
@@ -78,26 +87,48 @@
         public double Bs
         { get { return bs; } }
 
-        double GovtRules.EmployeePF(double basicSalary)
+        public double EmployeePF(double basicSalary)
         {
-            throw new NotImplementedException();
+            return 0.12 * basicSalary;
         }
 
-        double GovtRules.gratuityAmount(float serviceCompleted, double basicSalary)
+        public double gratuityAmount(float serviceCompleted, double basicSalary)
         {
-            throw new NotImplementedException();
+            return 0;
         }
 
-        string GovtRules.LeaveDetails()
+        public string LeaveDetails()
         {
-            throw new NotImplementedException();
+            return "Casual Leave: 2 days/month\nSick Leave: 5 days/year\nPrivilege Leave: 5 days/year";
         }
     }
-    internal class Program
-    {      
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+}
+internal class Test_Program
+{
+    static void Main(string[] args)
+    {
+        Program.TCS tcsEmployee = new Program.TCS(1, "John", "IT", "Developer", 50000);
+        Program.Accenture accentureEmployee = new Program.Accenture(2, "Alice", "HR", "Manager", 60000);
+
+        Console.WriteLine("TCS Employee Details:");
+        Console.WriteLine($"Employee ID: {tcsEmployee.empid}");
+        Console.WriteLine($"Name: {tcsEmployee.Name}");
+        Console.WriteLine($"Department: {tcsEmployee.Dept}");
+        Console.WriteLine($"Designation: {tcsEmployee.Desg}");
+        Console.WriteLine($"Basic Salary: {tcsEmployee.bs:C}");
+        Console.WriteLine($"PF Contribution: {tcsEmployee.EmployeePF(tcsEmployee.bs):C}");
+        Console.WriteLine($"Leave Details: {tcsEmployee.LeaveDetails()}");
+        Console.WriteLine($"Gratuity Amount: {tcsEmployee.gratuityAmount(10, tcsEmployee.bs):C}");
+
+        Console.WriteLine("\nAccenture Employee Details:");
+        Console.WriteLine($"Employee ID: {accentureEmployee.empid}");
+        Console.WriteLine($"Name: {accentureEmployee.Name}");
+        Console.WriteLine($"Department: {accentureEmployee.Dept}");
+        Console.WriteLine($"Designation: {accentureEmployee.Desg}");
+        Console.WriteLine($"Basic Salary: {accentureEmployee.bs:C}");
+        Console.WriteLine($"PF Contribution: {accentureEmployee.EmployeePF(accentureEmployee.bs):C}");
+        Console.WriteLine($"Leave Details: {accentureEmployee.LeaveDetails()}");
+        Console.WriteLine($"Gratuity Amount: {accentureEmployee.gratuityAmount(8, accentureEmployee.bs):C}");
     }
+}
 }
