faf6c10dd62ecfc712c49ab5084d6321376f2442
mubeena <95677268+Mubeenamube@users.noreply.github.com>
2024-01-04 21:37:04 +0530

    Add files via upload

---
+++ APIAssignment/HandsOnAssignment/HandsOnAssignment/Controllers/BookController.cs
@@ -0,0 +1,155 @@
+﻿using HandsOnAssignment.Model;
+using HandsOnAssignment.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HandsOnAssignment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly BookRepository rep;
+
+        public BookController()
+        {
+            rep = new BookRepository();
+        }
+        [HttpPost,Route("AddBook")]
+        public IActionResult AddBook(Book book)
+        {
+            try
+            {
+                rep.AddBook(book);
+                return StatusCode(200, book);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(501, ex);
+            }
+        }
+
+        [HttpGet, Route("GetAllBook")]
+        public IActionResult GetAllBook()
+        {
+            try
+            {
+                List<Book> books = rep.GetAllBook();
+                return StatusCode(200, books);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(501, ex);
+            }
+        }
+
+        [HttpDelete, Route("DeleteBook/{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            try
+            {
+                rep.DeleteBook(id);
+                return StatusCode(200, "Teacher Deleted");
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(501, ex);
+            }
+        }
+
+        [HttpPut, Route("EditBook")]
+        public IActionResult EditBook(Book book)
+        {
+            try
+            {
+                rep.EditBook(book);
+                return StatusCode(200, book);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(501, ex);
+            }
+        }
+
+        [HttpGet, Route("GetBookByAuthor/{author}")]
+        public IActionResult GetBookByAuthor(string author)
+        {
+            try
+            {
+                return StatusCode(200, rep.GetBookByAuthor(author));
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(501, ex);
+            }
+        }
+
+
+        [HttpGet, Route("GetBookByLang/{lang}")]
+        public IActionResult GetBookByLang(string lang)
+        {
+            try
+            {
+                return StatusCode(200, rep.GetBookByLang(lang));
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(501, ex);
+            }
+        }
+
+        [HttpGet, Route("GetBookByYear/{year}")]
+        public IActionResult GetBookByYear(int year)
+        {
+            try
+            {
+                return StatusCode(200, rep.GetBookByYear(year));
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(501, ex);
+            }
+        }
+        [HttpGet,Route("GetById/{id}")]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                Book book = rep.GetBookById(id);
+                if (book != null)
+                    return StatusCode(200, book);
+                else
+                    return StatusCode(404, new JsonResult("Invalid book Id")); 
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(501, ex);
+            }
+        }
+        [HttpGet,Route("GetByName/{name}")]
+        public IActionResult GetByName(string name)
+        {
+            try
+            {
+                Book book = rep.GetBookByName(name);
+                if (book != null)
+                    return StatusCode(200, book);
+                else
+                    return StatusCode(404, new JsonResult("Invalid book name"));
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(501, ex);
+            }
+        }
+    }
+}
---
+++ APIAssignment/HandsOnAssignment/HandsOnAssignment/Repository/BookRepository.cs
@@ -0,0 +1,170 @@
+﻿using HandsOnAssignment.Model;
+
+namespace HandsOnAssignment.Repository
+{
+    public class BookRepository : IBook
+    {
+        public static List<Book> books = new List<Book>();
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+
+        public void DeleteBook(int id)
+        {
+            try
+            {
+                foreach (var item in books)
+                {
+                    if (item.Id == id)
+                    {
+                        books.Remove(item);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void EditBook(Book book)
+        {
+            try
+            {
+                foreach (var item in books)
+                {
+                    if (item.Id == book.Id)
+                    {
+                        item.Name = book.Name;
+                        item.Price = book.Price;
+                        item.Author = book.Author;
+                        item.Releaseyear = book.Releaseyear;
+                        item.Publisher = book.Publisher;
+                        break;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<Book> GetAllBook()
+        {
+            try
+            {
+                return books;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<Book> GetBookByAuthor(string author)
+        {
+            try
+            {
+                List<Book> bookbyauthor = new List<Book>();
+                foreach (var item in books)
+                {
+                    if (item.Author == author)
+                    {
+                        bookbyauthor.Add(item);
+                    }
+                }
+                return bookbyauthor;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public Book GetBookById(int id)
+        {
+            try
+            {
+                foreach (var item in books)
+                {
+                    if (item.Id == id)
+                        return item;
+                }
+                return null;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<Book> GetBookByLang(string lang)
+        {
+            try
+            {
+                List<Book> bookbylang = new List<Book>();
+                foreach (var item in books)
+                {
+                    if (item.Lang == lang)
+                    {
+                        bookbylang.Add(item);
+                    }
+                }
+                return bookbylang;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public Book GetBookByName(string name)
+        {
+            try
+            {
+                foreach (var item in books)
+                {
+                    if(item.Name == name)
+                    {
+                        return item;
+                    }
+                }
+                return null;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<Book> GetBookByYear(int year)
+        {
+            try
+            {
+                List<Book> bookbyyear = new List<Book>();
+                foreach (var item in books)
+                {
+                    if (item.Releaseyear == year)
+                    {
+                        bookbyyear.Add(item);
+                    }
+                }
+                return books;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
